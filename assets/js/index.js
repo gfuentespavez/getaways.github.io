a28548ab8e63713e69c58730f61836d164d21140
gfuentespavez <160777049+gfuentespavez@users.noreply.github.com>
2024-04-14 15:47:10 -0400

    added: button to form with toggle in JS, navbar with solid-bg when scrolled

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,19 @@ gridCheck.addEventListener("change", function() {
     funLabel.textContent = "Check for fun!";
   }
 });
+$(document).ready(function() {
+  $(".more-info button").click(function() {
+    $(this).text($(this).text() === "Abre el formulario" ? "Cierra el formulario" : "Abre el formulario");
+    $(this).parent().siblings(".container-fluid").toggle();
+  });
+});
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    navbar.classList.add('solid-bg'); // Add solid-bg class when scrolled
+  } else {
+    navbar.classList.remove('solid-bg'); // Remove solid-bg class when at top
+  }
+});
